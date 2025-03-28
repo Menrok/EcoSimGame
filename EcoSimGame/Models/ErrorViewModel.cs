618a34992e6d8aaa1f8b34150188e0034bba6430
Menrok <sz.mi@interia.pl>
2025-03-28 10:00:19 +0100

    0.0.1
    Create project EconomySimulatorGame

---
+++ EcoSimGame/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EcoSimGame.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
