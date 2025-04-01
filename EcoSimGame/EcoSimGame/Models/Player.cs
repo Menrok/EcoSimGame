3f70c12deac13541b77f70ec5dfe1c4d2202db30
Menrok <sz.mi@interia.pl>
2025-04-01 15:17:45 +0200

    0.0.5
    Blazor

---
+++ EcoSimGame/EcoSimGame/Models/Player.cs
@@ -0,0 +1,20 @@
+﻿namespace EcoSimGame.Models;
+
+public class Player
+{
+    public decimal Money { get; set; }
+    public Dictionary<string, int> Materials { get; set; }
+    public int IronBars { get; set; } 
+    public DateTime? LastProcessingTime { get; set; }
+
+    public Player()
+    {
+        Money = 100.0m;
+        Materials = new Dictionary<string, int>
+        {
+            { "Drewno", 0 },
+            { "Ruda Żelaza", 0 }
+        };
+        IronBars = 0;
+    }
+}
