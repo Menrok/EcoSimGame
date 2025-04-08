c832e4404446978f22f34347f0a04ccb09b2c462
Menrok <sz.mi@interia.pl>
2025-04-08 18:44:34 +0200

    0.0.9

--- EcoSimGame/Models/List/MaterialList.cs
+++ EcoSimGame/Models/List/MaterialList.cs
@@ -5,20 +5,19 @@ public class MaterialList
     public static List<Material> AllMaterials { get; } = new()
         {
             new Material("Drewno", 10),
-            new Material("Ruda żelaza", 15),
-            new Material("Miedź", 20),
-            new Material("Cyna", 25),
-            new Material("Ruda złota", 50),
-            new Material("Ruda srebra", 30),
-            
-            new Material("Sztabka żelaza", 40, "Przetapianie żelaza"),
-            new Material("Sztabka mosiądzu", 60, "Przetapianie mosiądzu"),
-            new Material("Sztabka stali", 80, "Przetapianie stali"),
-            new Material("Sztabka złota", 120, "Przetapianie złota"),
-            new Material("Sztabka srebra", 100, "Przetapianie srebra"),
-            new Material("Sztabka platyny", 150, "Przetapianie platyny"),
-            
-            new Material("Sztabka mithrilu", 200, "Przetapianie mithrilu")
+            new Material("Deski", 30),
+
+            new Material("Ruda żelaza", 10),
+            new Material("Sztabka żelaza", 30),
+
+            new Material("Glina", 10),
+            new Material("Cegła", 30),
+
+            new Material("Len", 10),
+            new Material("Płótno", 30),
+
+            new Material("Narzędzia", 200, "Produkcja narzędzi"),
+            new Material("Odzież", 200, "Produkcja odzieży"),
         };
 
     public static Material GetByName(string name) =>
--- EcoSimGame/Models/ProductionTree.cs
+++ EcoSimGame/Models/ProductionTree.cs
@@ -8,13 +8,8 @@ public class ProductionTree
     {
         Nodes = new List<ProductionNode>
         {
-            new ProductionNode("Przetapianie żelaza", 50, null, 100),
-            new ProductionNode("Przetapianie mosiądzu", 100, "Przetapianie żelaza", 150),
-            new ProductionNode("Przetapianie stali", 200, "Przetapianie mosiądzu", 200),
-            new ProductionNode("Przetapianie złota", 300, "Przetapianie stali", 300),
-            new ProductionNode("Przetapianie srebra", 250, "Przetapianie stali", 250),
-            new ProductionNode("Przetapianie platyny", 400, "Przetapianie srebra", 350),
-            new ProductionNode("Przetapianie mithrilu", 500, "Przetapianie platyny", 500)
+            new ProductionNode("Narzędzia", 100, "Produkcja narzędzi", 1000),
+            new ProductionNode("Odzież", 100, "Produkcja Odzieży", 1000),
         };
     }
 }
